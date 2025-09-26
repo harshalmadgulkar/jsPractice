e40d1107609bed065abbfa09c4c5077647802175
Harshal <harshal.madgulkar725@gmail.com>
2025-09-26 17:21:50 +0530

    Approach 2

--- lecture2.js
+++ lecture2.js
@@ -1,4 +1,5 @@
 // 1. create function which will check passed parameter is Array or not
+// Approach 1
 // function checkArray(input) {
 // 	return Array.isArray(input);
 // }
@@ -7,3 +8,10 @@
 // console.log(checkArray({}));
 // console.log(checkArray([]));
 // console.log(checkArray([1, 2, 3, 4]));
+
+// Approach 2
+// function checkArray(input) {
+// 	return input instanceof Array;
+// }
+
+// console.log(checkArray([1]));
